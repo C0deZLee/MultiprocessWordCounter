7b5216eeea57b2b7c42b562636dab9b684bbd8bf
steveleelx <steveleelx@gmail.com>
2016-03-18 19:34:34 -0400

    try to fix pipe

--- wordc_mp.c
+++ wordc_mp.c
@@ -37,35 +37,35 @@ int add_to_list(char *val) {
     if (HEAD == NULL)
         return (create_list(val));
 
-    struct word_count *curr = (word_count*) malloc (sizeof(word_count));
-    curr->word = val, curr->count = 1, curr->last = NULL, curr->next = NULL;
+    struct word_count *s_val = (word_count*) malloc (sizeof(word_count));
+    s_val->word = val, s_val->count = 1, s_val->last = NULL, s_val->next = NULL;        // create new struct
 
-    for(struct word_count *search=HEAD; search!=NULL; search=search->next){            // start at the first element
-        if (strcmp (curr->word, search->word) < 0){                                    // put front
-            curr->next = search;
-            curr->last = search->last;
-            if(search->last != NULL) {
-                search->last->next = curr;
-                search->last = curr;
+    for(struct word_count *curr=HEAD; curr!=NULL; curr=curr->next) {                    // start at the first element
+        if (strcmp (s_val->word, curr->word) < 0) {                                     // s_val<curr, insert front
+            s_val->next = curr;
+            s_val->last = curr->last;
+            if(curr->last != NULL) {
+                curr->last->next = s_val;
+                curr->last = s_val;
             }
             else {
-                search->last = curr;
-                HEAD = curr;
+                curr->last = s_val;
+                HEAD = s_val;
             }
             return 0;
         }
 
-        if(strcmp (curr->word, search->word)>0 &&
-           (search->next == NULL || strcmp(curr->word, search->next->word)<0)){       // put behind
-            curr->next = search->next;
-            curr->last = search;
-            if(search->next != NULL) {
-                search->next->last = curr;
-                search->next = curr;
+        else if(strcmp (s_val->word, curr->word)>0 &&
+           (curr->next == NULL || strcmp(s_val->word, curr->next->word)<0)) {       // s_val>curr, insert behind
+            s_val->next = curr->next;
+            s_val->last = curr;
+            if(curr->next != NULL) {
+                curr->next->last = s_val;
+                curr->next = s_val;
             }
             else {
-                search->next = curr;
-                TAIL = curr;
+                curr->next = s_val;
+                TAIL = s_val;
             }
             return 0;
         }
@@ -90,6 +90,43 @@ int search_in_list(char *val) {
     return 0;
 }
 
+/* merge the lists together*/
+int merge_list(word_count *s_val){
+    for(struct word_count *curr = HEAD; curr != NULL; curr = curr->next){
+        if(strcmp (s_val->word, curr->word) == 0) {                          // val == curr
+            curr->count += s_val->count;
+        }
+        else if(strcmp (s_val->word, curr->word) < 0) {                     // val < curr
+            s_val->next = curr;
+            s_val->last = curr->last;
+            if(curr->last != NULL) {
+                curr->last->next = s_val;
+                curr->last = s_val;
+            }
+            else {
+                curr->last = s_val;
+                HEAD = s_val;
+            }
+            return 0;
+        }
+
+        else if(strcmp (s_val->word, curr->word)>0 &&
+                (curr->next == NULL || strcmp(s_val->word, curr->next->word)<0)) {       // s_val>curr, insert behind
+            s_val->next = curr->next;
+            s_val->last = curr;
+            if(curr->next != NULL) {
+                curr->next->last = s_val;
+                curr->next = s_val;
+            }
+            else {
+                curr->next = s_val;
+                TAIL = s_val;
+            }
+            return 0;
+        }
+    }
+}
+
 /* Remove punctuation and make all lowercase */
 char *word_format(char *raw_str) {
 
@@ -98,10 +135,7 @@ char *word_format(char *raw_str) {
     while (*raw_str) {
         if (ispunct(*raw_str))                                           // skip it
             raw_str++;
-        else if (isupper(*raw_str))
-            *formatted++ = tolower(*raw_str++);                          // make it lowercase
-        else
-            *formatted++ = *raw_str++;                                   // increment both pointers and copying
+        else *formatted++ = (char) (isupper(*raw_str) ? tolower(*raw_str++) : *raw_str++);// increment both pointers and copying
     }
     return final;
 }
@@ -158,11 +192,10 @@ int main(int argc, char *argv[]) {
 		status,                                                        // the status automatically points to the exit position of child process
 		partial_num_of_words;                                          // get the partial size for every child process
 
-	int **fd;//this is an array of pointers. Every process has fd[0-2]. A way to identify pipes is to have fd[0-2][0-n]
+	int **fd;                                                          // this is an array of pointers. Every process has fd[0-2]. A way to identify pipes is to have fd[0-2][0-n]
+    fd = (int**)malloc(TOTAL_PROCESS_NUM * sizeof(int*));              // allocates space for needed pointers
 
-	fd = (int**)malloc(TOTAL_PROCESS_NUM * sizeof(int*));//Allocates space for TOTAL_PROCESS_NUM pointers
-
-	//TEMP: fd = (int**)malloc( argv[4] * sizeof(int*));//Allocates space for 4 pointers
+	//TEMP: fd = (int**)malloc( argv[4] * sizeof(int*));// allocates space for needed pointers
 
 	for (int i = 0; i < TOTAL_PROCESS_NUM - 1; i++) {
 		fd[i] = (int*)malloc(2 * sizeof(int));
@@ -175,9 +208,7 @@ int main(int argc, char *argv[]) {
 		pipe(fd[i]);
 	}*/
 
-
     pid_t pid;                                                         // set up multi-process
-
 
     while(fscanf(fp, "%s", raw_str) != EOF) {                          // tokenize the file and save it to tokenized_file
         tokenized_file[total_num_of_words] = word_format(raw_str);     // format the word
@@ -187,82 +218,68 @@ int main(int argc, char *argv[]) {
 	//TEMP: partial_num_of_words = total_num_of_words / (int)argv[4];
 
 
-	for (int i = 1; i <= TOTAL_PROCESS_NUM - 1; i++) { //the number of children is TOTAL_PROCESS_NUM - 1
-        //TEMP:for (int i=1; i<=(int)argv[4] -1; i++) {                   // the 4th arg is number of processes
+	for (int i = 1; i <= TOTAL_PROCESS_NUM - 1; i++) {                 //the number of children is TOTAL_PROCESS_NUM - 1
+        //TEMP:for (int i=1; i<=(int)argv[4] -1; i++) {                // the 4th arg is number of processes
+
 		pid = fork();                                                  // create new process
 
         if (pid == 0) {                                                // This is a CHILD process
             for (int j = partial_num_of_words*i; j < partial_num_of_words*(i + 1); j++) {
                 search_in_list(tokenized_file[j]);
             }
+
             printf("This is a CHILD process, %d \n", i);
-            // TODO: pipe the result to parent
-			close(fd[0][i]); //close read end
-            // Steve: the sorted list is not a file, it is a linked list, and each process has one (they are independent),
-            //        you can access the linked list by HEAD->next, HEAD->word
-            //        one possible solution is send the HEAD pointer through pipe
-
-			/*
-			 for (struct word_count *curr = HEAD; curr->next != NULL; curr = curr->next){
-				write(fd[1][i-1], curr , sizeof(curr)); 
-			 }
-			*/
+			close(fd[i][0]); //close read end
 			 
-			for (struct word_count *curr = HEAD; curr->next != NULL; curr = curr->next){
-				char *num = (char *) (curr->count + '0');    //By adding '0' a number becomes a char http://stackoverflow.com/questions/2279379/how-to-convert-integer-to-char-in-c
-				                                     //There should be no number with more than 70 digits
-				/*char *num;
-				sprintf(num, "%d", curr->count);//takes count, makes into a char, sends it to num
-				This is just another way to make int into char*/
+			for (struct word_count *curr = HEAD; curr != NULL; curr = curr->next){
+                write(fd[i][0], curr, strlen(curr));
+                //char *num = (char *) (curr->count + '0');    //By adding '0' a number becomes a char http://stackoverflow.com/questions/2279379/how-to-convert-integer-to-char-in-c
+				                                                        //There should be no number with more than 70 digits
 
-				write(fd[0][i], curr->word, strlen(curr->word));      //sends word
-				write(fd[0][i], num, strlen(num));                    //sends number
+
+				//write(fd[i][0], curr->word, strlen(curr->word));      //sends word
+				//write(fd[i][0], num, strlen(num));                    //sends number
 			}
-			 
-			 close(fd[1][i]); //close write end
+			 write(fd[i][0], NULL, sizeof(NULL));
+			 close(fd[i][1]); //close write end
 				 //when read looks like word1\0 5\0 so read stops every time when it hits null
             exit(0);                                                   // exit the child process since it done all work
         }
+
+
 
         else if (pid < 0) {                                            // not a child, not a parent, then error
             printf("Failed to create new processes");
             exit(1);
         }
     }
-
-    if (pid > 0) {                                                     // This is the PARENT process
-        for (int j=0; j<partial_num_of_words; j++) {                   // first, parent sort first part, which is tokenized_file[0] - tokenized_file[partial_num_of_words]
-            search_in_list(tokenized_file[j]);
-        }
-
-        while (wait(&status)>0);                                       // then wait all children done their work
-        printf("This is the PARENT process\n");
-        // TODO: get the result from child
-		int bytesRead;
-		char buffer[10000*sizeof(HEAD)];                   //must be able to hold the nodes passed
-		close(fd[1][0]); //close write end of parent
-		for (int i = 1; i <= TOTAL_PROCESS_NUM - 1; i++) {
-			//read itself is blocking
-            //TEMP:for (int i=1; i<=(int)argv[4]; i++) {                   // the 4th arg is number of processes
-			if ((bytesRead = read(fd[0][0], buffer, strlen(buffer))) >= 0){ //size is the bytes of the file.
-				//read returns the number of bytes read. It returns 0 at end-of-file
-				char word, number;
-				word = buffer[0];
-				number = buffer[1];
-				struct word_count *newWord = (word_count*) malloc (sizeof(word_count));
-				newWord->word = (char *) word;
-				newWord->count = (int) number-'0';//change back to int
-				add_to_list(newWord);
-				//reads a number of bytes from the file associated with fd and places the characters read into buffer
-			}
-        }
-		close(fd[0][0]); //close read end of parent
-        // TODO: put them together
-        // TODO: sort
-        // TODO: print result
+    // here below is parent
+    for (int j=0; j<partial_num_of_words; j++) {                   // first, parent sort first part, which is tokenized_file[0] - tokenized_file[partial_num_of_words]
+        search_in_list(tokenized_file[j]);
     }
 
+    printf("This is the PARENT process\n");
+    struct word_count *buffer = (word_count *) malloc(sizeof(word_count));//must be able to hold the nodes passed
+    close(fd[0][1]);                                               //close write end of parent
+    for (int i = 1; i <= TOTAL_PROCESS_NUM-1; i++) {
+        //TEMP:for (int i=1; i<=(int)argv[4]; i++) {                   // the 4th arg is number of processes
+        read(fd[i][0], buffer, strlen(buffer));
 
+            //read returns the number of bytes read. It returns 0 at end-of-file
+            printf("testing message: bytes_read is %d, buffer is %s", buffer->count);
+//                buffer[bytes_read] = 0;
+//				char* word, number;
+//				word = buffer[0];
+//				number = buffer[1];
+//				struct word_count *new_word = (word_count*) malloc (sizeof(word_count));
+//				new_word->word = (char *) word;
+//				new_word->count = (int) number - '0';//change back to int
+//                merge_list(new_word);
+            //reads a number of bytes from the file associated with fd and places the characters read into buffer
+
+    }
+    close(fd[0][0]); //close read end of parent
+    // TODO: print result
     gettimeofday(&end, NULL);                                          // get the total runtime
     long runtime = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec));
 
