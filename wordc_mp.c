5d2e4af9b7251a602f91ebc51d9db0650ed7864a
steveleelx <steveleelx@gmail.com>
2016-03-18 23:21:11 -0400

    finally workable version

--- wordc_mp.c
+++ wordc_mp.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <errno.h>
+#include <limits.h>
 
 // 1 is for write
 
@@ -21,7 +20,7 @@ typedef struct word_count {
 /* Global variable */
 word_count *HEAD = NULL;
 word_count *TAIL = NULL;
-int argv[4] = 8;
+int TOTAL_PROCESS_NUM = 8;
 
 /* reate a new list if we don't have a list */
 int create_list(char *val) {
@@ -40,10 +39,10 @@ int add_to_list(char *val) {
         return (create_list(val));
 
     struct word_count *s_val = (word_count*) malloc (sizeof(word_count));
-    s_val->word = val, s_val->count = 1, s_val->last = NULL, s_val->next = NULL;      // create new struct
+    s_val->word = val, s_val->count = 1, s_val->last = NULL, s_val->next = NULL;        // create new struct
 
-    for(struct word_count *curr=HEAD; curr!=NULL; curr=curr->next) {                  // start at the first element
-        if (strcmp (s_val->word, curr->word) < 0) {                                   // s_val<curr, insert front
+    for(struct word_count *curr=HEAD; curr!=NULL; curr=curr->next) {                    // start at the first element
+        if (strcmp (s_val->word, curr->word) < 0) {                                     // s_val<curr, insert front
             s_val->next = curr;
             s_val->last = curr->last;
             if(curr->last != NULL) {
@@ -58,7 +57,7 @@ int add_to_list(char *val) {
         }
 
         else if(strcmp (s_val->word, curr->word)>0 &&
-           (curr->next == NULL || strcmp(s_val->word, curr->next->word)<0)) {       // s_val>curr, insert behind
+                (curr->next == NULL || strcmp(s_val->word, curr->next->word)<0)) {       // s_val>curr, insert behind
             s_val->next = curr->next;
             s_val->last = curr;
             if(curr->next != NULL) {
@@ -81,13 +80,13 @@ int search_in_list(char *val) {
     if (HEAD == NULL)
         return (create_list(val));
 
-    for (struct word_count *curr = HEAD; curr != NULL; curr = curr->next) {// search from the HEAD
+    for (struct word_count *curr = HEAD; curr != NULL; curr = curr->next) {  // search from the HEAD
         if (strcmp(curr->word, val) == 0) {
             curr->count++;
             return 0;
         }
     }
-    add_to_list(val);                                                     // not in list then add to list
+    add_to_list(val);                                                        // not in list then add to list
 
     return 0;
 }
@@ -149,10 +148,10 @@ char *word_format(char *raw_str) {
 /* print the result into files */
 void print_result(char *result_filename, char *runtime_filename, long runtime, char **file, int w){
     FILE *fp;                                                           // create the file pointer
-    fp = fopen(result_filename, "w+");                                  // print the result into result_filename
+    fp = fopen(result_filename, "w+");                                  // assign the file pointer
 
     for(int i=0; i<w; i++) search_in_list(file[i]);
-    for(struct word_count *curr = HEAD;curr != NULL;curr = curr->next)
+    for(struct word_count *curr = HEAD;curr != NULL;curr = curr->next)  // print the result into result_filename
         fprintf(fp,"%s, %d\n", curr->word, curr->count);
 
     fp = fopen(runtime_filename, "w+");                                 // print the runtime into runtime_filename
@@ -162,14 +161,25 @@ void print_result(char *result_filename, char *runtime_filename, long runtime, c
 
 /* main function start here */
 int main(int argc, char *argv[]) {
-    if (argc != 5){
-        if ((argc == 2 && (argv[1] == "--help" || argv[1] == "-h")) || argc == 1)  // help
-            printf("./wordc-mp input-textfile output-countfile output-runtime num-of-processes\n");
-        else
-            printf("Expected 4 arguments, given %d", argc-1);
+    if ((argc == 2 && (argv[1] == "--help" || argv[1] == "-h")) || argc == 1){  // help
+        printf("./wordc-mp input-textfile output-countfile output-runtime num-of-processes\n");
         exit(1);
     }
+    if (argc != 5){
+        printf("Expected 4 arguments, given %d", argc-1);              // no enough argv
+        exit(1);
+    }
+    char *p;
+    errno = 0;
+    int conv = strtol(argv[4], &p, 10);
+    if (errno != 0 || *p != '\0' || conv > INT_MAX) {
+        printf("The 4th argument should be a number!\n");              // error on argv[4]
+        exit(1);
+    } else {
+        TOTAL_PROCESS_NUM = conv;                                      // No error for argv[4]
+    }
 
+    //--------------------PROGRAM START HERE---------------------
     struct timeval start, end;                                         // set the start time
     gettimeofday(&start, NULL);
 
@@ -184,17 +194,17 @@ int main(int argc, char *argv[]) {
     fseek(fp, 0L, SEEK_SET);                                           // seek back to beginning of file
 
     char *raw_str = (char *) malloc (128 * sizeof(char)),              // save the unformatted word
-         **tokenized_file = (char **) malloc (size * sizeof(char*));   // save the file into a string array
+            **tokenized_file = (char **) malloc (size * sizeof(char*));   // save the file into a string array
 
-	int total_num_of_words = 0;                                        // get the total length of the file
+    int total_num_of_words = 0;                                        // get the total length of the file
 
-	int **fd;                                                          // this is an array of pointers. Every process has fd[0-2]. A way to identify pipes is to have fd[0-2][0-n]
-    fd = (int**)malloc((int)argv[4] * sizeof(int*));                   // allocates space for needed pointers
+    int **fd;                                                          // this is an array of pointers. Every process has fd[0-2]. A way to identify pipes is to have fd[0-2][0-n]
+    fd = (int**)malloc(TOTAL_PROCESS_NUM * sizeof(int*));              // allocates space for needed pointers
 
-	for (int i = 0; i < (int)argv[4]; i++) {                           // create pipe from 0 to argv[4]-1
-		fd[i] = (int*)malloc(2 * sizeof(int));
-		pipe(fd[i]);
-	}
+    for (int i = 0; i < TOTAL_PROCESS_NUM; i++) {                      // create pipe from 0 to TOTAL_PROCESS_NUM-1
+        fd[i] = (int*)malloc(2 * sizeof(int));
+        pipe(fd[i]);
+    }
 
     pid_t pid;                                                         // set up multi-process
 
@@ -202,17 +212,17 @@ int main(int argc, char *argv[]) {
         tokenized_file[total_num_of_words] = word_format(raw_str);     // format the word
         total_num_of_words++;                                          // get the number of total words in file
     }
-    int partial_num_of_words = total_num_of_words / (int) argv[4];     // get the number of word for every child process
+    int partial_num_of_words = total_num_of_words / TOTAL_PROCESS_NUM;
 
-	for (int i = 1; i < (int)argv[4]; i++) {                           //the number of children is 1 to argv[4]-1
-		pid = fork();                                                  // create new process
+    for (int i = 1; i < TOTAL_PROCESS_NUM; i++) {                      //the number of children is 1 to TOTAL_PROCESS_NUM-1
+        pid = fork();                                                  // create new process
         if (pid == 0) {                                                // This is a CHILD process
             close(fd[i][0]);                                           // close read end (no need)
             for (int j = partial_num_of_words*i; j < partial_num_of_words*(i + 1); j++)
                 search_in_list(tokenized_file[j]);                     // each child search its own part
 
 
-			for (struct word_count *curr = HEAD; curr != NULL; curr = curr->next)
+            for (struct word_count *curr = HEAD; curr != NULL; curr = curr->next)
                 write(fd[i][1], curr, 1000);                           // pipe the data to parent
 
 
@@ -238,7 +248,7 @@ int main(int argc, char *argv[]) {
 
     struct word_count *buffer = (word_count *) malloc (sizeof(word_count));
     close(fd[0][1]);                                                  // close write end of parent
-    for (int i = 1; i < (int)argv[4]; i++) {                          // child process is 1 to total-1
+    for (int i = 1; i < TOTAL_PROCESS_NUM; i++) {                     // child process is 1 to total-1
         read(fd[i][0], buffer, 1000);
 
         while(buffer->count != -1){
@@ -255,7 +265,7 @@ int main(int argc, char *argv[]) {
 
     long runtime = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec));
     printf("Done. Total runtime: %ld\nThe result is in %s, and the runtime is in %s\n", runtime, argv[2], argv[3]);
-    print_result(argv[2], argv[3],runtime,tokenized_file,total_num_of_words);
+    print_result(argv[2], argv[3],runtime,tokenized_file,total_num_of_words);// print the result
 
     fclose(fp);                                                        // close the file
 
